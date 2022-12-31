26165a38daf53c580ca6076148776666be4e9c70
EC2 Default User <ec2-user@ip-172-31-25-212.ap-northeast-1.compute.internal>
2022-12-31 14:58:38 +0000

    [Update]Task_02_DMM

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,13 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books =Book.all
+    @book =Book.new
+  end
+  
+  def create
+    book =Book.new(book_params)
+    book.save
   end
 
   def show
